01ce30751016853ed1df385a7c82ce355cd2af34
grantferowich <g.ferowich@gmail.com>
2023-05-08 16:08:08 -0500

    updated ip address list

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ mongoose.set('strictQuery', false);
 mongoose.connect(mongoStr, { useNewUrlParser: true})
 const db = mongoose.connection;
 
+
 db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('Connected to Mongoose Database'))
 
