8caee14086941386e31993b7aef62531b5c3dff7
grantferowich <g.ferowich@gmail.com>
2023-05-30 13:38:03 -0500

    removed temporary /deleteAll route

--- api/baskets.js
+++ api/baskets.js
@@ -98,14 +98,16 @@ router.delete('/delete/:id', async (request, response) => {
 
 // Delete all baskets
 // from Mr. ChatGPT
-router.delete('/deleteAll', async (request, response) => {
-    try {
-      await Basket.deleteMany(); // Delete all documents in the "Basket" collection
-      response.json({ message: 'All baskets deleted successfully.' });
-    } catch (error) {
-      response.status(500).json({ message: error.message });
-    }
-  });
+// Successfully deleted all baskets on May 30, 2023.
+
+// router.delete('/deleteAll', async (request, response) => {
+//     try {
+//       await Basket.deleteMany(); // Delete all documents in the "Basket" collection
+//       response.json({ message: 'All baskets deleted successfully.' });
+//     } catch (error) {
+//       response.status(500).json({ message: error.message });
+//     }
+//   });
 
 function logger(request, response, next) {
     console.log(request.originalUrl)
---
+++ devCode.js
@@ -0,0 +1,12 @@
+// Delete all baskets
+// from Mr. ChatGPT
+// Successfully deleted all baskets on May 30, 2023.
+
+// router.delete('/deleteAll', async (request, response) => {
+//     try {
+//       await Basket.deleteMany(); // Delete all documents in the "Basket" collection
+//       response.json({ message: 'All baskets deleted successfully.' });
+//     } catch (error) {
+//       response.status(500).json({ message: error.message });
+//     }
+//   });
