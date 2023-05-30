3c7e4b80e59859445405df77782e645c6f98bf1e
grantferowich <g.ferowich@gmail.com>
2023-05-30 13:43:52 -0500

    updated access-control-allow-origin list in vercel.json

--- api/baskets.js
+++ api/baskets.js
@@ -96,18 +96,6 @@ router.delete('/delete/:id', async (request, response) => {
     }
 })
 
-// Delete all baskets
-// from Mr. ChatGPT
-// Successfully deleted all baskets on May 30, 2023.
-
-// router.delete('/deleteAll', async (request, response) => {
-//     try {
-//       await Basket.deleteMany(); // Delete all documents in the "Basket" collection
-//       response.json({ message: 'All baskets deleted successfully.' });
-//     } catch (error) {
-//       response.status(500).json({ message: error.message });
-//     }
-//   });
 
 function logger(request, response, next) {
     console.log(request.originalUrl)
