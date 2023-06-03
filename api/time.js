d47c2fb6e30068ecbd83aa93de89f96cac33b405
grantferowich <g.ferowich@gmail.com>
2023-06-03 17:40:55 -0500

    created the /time api

--- api/time.js
+++ api/time.js
@@ -2,3 +2,25 @@ const express = require('express');
 const router = express.Router();
 const Time = require('../models/time')
 
+router.get('/time', (request, response) => {
+    Time.findOne({}, {}, {sort: { lastUpdate: -1 }})
+    .then(time => {
+        response.json(time)
+    })
+    .catch(error => {
+        console.error('Error retrieving lastUpdate:', error);
+        response.status(500).send('An error occurred.')
+    })
+})
+
+router.post('/time', (request, response) => {
+    const newTime = new Time();
+    newTime.save()
+    .then(() => {
+        response.send('New lastUpdate was successfully added.');
+    })
+    .catch(error => {
+        console.error('Error adding new lastUpdate:', error);
+        response.status(500).send('An error occurred.')
+    })
+})
