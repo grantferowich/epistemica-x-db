cf6781c4f8dd40ee00c345892e09e2340f97de61
grantferowich <g.ferowich@gmail.com>
2023-06-03 13:46:06 -0500

    exported router from the Coins endpoint

--- api/coins.js
+++ api/coins.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Coin = requires('../models/coin');
 const Time = requires('../models/time');
 const cors = require('cors');
-const { request } = require('..');
+
 
 router.get('/getAll', cors(), async (request, response) => {
     try {
@@ -16,7 +16,20 @@ router.get('/getAll', cors(), async (request, response) => {
 })
 
 router.post('/post', async (request, response) => {
-    const coins = request.body;
-    await Time.findOneAndUpdate({}, { lastUpdated: Date.now()}, { upsert: true})
-    await Coin.insertMany(coins)
+    try {
+        // isolate 250 coins from api call
+        const coins = request.body;
+        // post to the Time endpoint 
+        await Time.findOneAndUpdate({}, { lastUpdated: Date.now()}, { upsert: true})
+        // post to the Coin endpoint
+        await Coin.insertMany(coins)
+
+        response.status(200).json(coins)
+    } catch (errorHM) {
+        console.log('Request Body', request.body)
+        response.status(400).json(console.log('Error!', errorHM))
+    }
+    
 })
+
+module.exports = router;
