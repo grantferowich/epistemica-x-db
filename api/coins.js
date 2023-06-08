184a33924f69c0bf2ab0a232f2ba78c585604f33
grantferowich <g.ferowich@gmail.com>
2023-06-08 16:46:47 -0500

    wrote data to a static file

--- api/coins.js
+++ api/coins.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Coin = require('../models/coin');
 const Time = require('../models/time');
 const cors = require('cors');
+const fs = require('fs');
 
 router.use(express.json());
 router.get('/getAll', cors(), async (request, response) => {
@@ -19,12 +20,22 @@ router.post('/post', async (request, response) => {
     try {
         // isolate 250 coins from api call
         const coins = request.body;
-        
-        // await redisClient.set('coins', coins)
+        const filePathStr = '/Users/knightoffaith/Desktop/Code/prodProjects/epistemica-x/src/data/tableData.json'
+        fs.writeFile(filePathStr, coins, ( error ) => {
+            if (error) {
+                console.error('Error writing file:', error);
+                response.status(500).send('An error occurred.');
+            } else {
+                console.log('Data written to file successfully.');
+                response.status(200).send('Data written to file.');
+            }
+        });
+
         // post to the Time endpoint 
         await Time.findOneAndUpdate({}, { lastUpdated: Date.now()}, { upsert: true});
         // post to the Coin endpoint
         await Coin.insertMany(coins)
+
         .then(() => {
             console.log('Coins were added to the database.');
             response.status(200).send('Coins were added successfully.')
@@ -51,11 +62,11 @@ router.get('/get250', async (req, res) => {
     }
   });
 
-  router.get('/get50', async (req, res) => {
+router.get('/get50', async (req, res) => {
     try {
       const coins = await Coin.find()
         .sort({ createdAt: -1 })
-        .limit(250);
+        .limit(50);
   
       res.json(coins);
     } catch (error) {
@@ -63,5 +74,4 @@ router.get('/get250', async (req, res) => {
       res.status(500).send('An error occurred.');
     }
   });
-
 module.exports = router;
