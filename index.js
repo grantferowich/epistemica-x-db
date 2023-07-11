cad606c5213ca6d9c64c9561d03ff70f9cdac15b
grantferowich <g.ferowich@gmail.com>
2023-07-11 06:36:35 -0500

    defined the completeAPIcall function

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const app = express();
 const mongoose = require('mongoose');
 const mongoStr = process.env.DATABASE_URL;
 const compression = require('compression');
+const axios = require('axious');
+const cron = require('node-cron');
+const Coin = require('./models/coin');
 
+// compression 
 app.use(compression({
     // Specify Brotli as the compression algorithm
     algorithm: 'brotli',
@@ -32,6 +36,26 @@ mongoose.connect(mongoStr, { useNewUrlParser: true}, {useMongoClient: true})
 const db = mongoose.connection;
 db.on('error', (error) => console.log(error));
 db.once('open', () => console.log('Connected to Mongoose Database'));
+
+const coinGeckoAPIStr = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=24h'
+const completeAPICall = async () => {
+    const responseHM = await axios.get(coinGeckoAPIStr)
+    try {
+        const apiDataArr = (responseHM.data.sort((a, b) => a.market_cap_rank - b.market_cap_rank));
+        await Coin.deleteMany();
+        await Coin.insertMany(apiDataArr);
+        console.log('API call was successfull.')
+    } catch (errorHM) {
+        console.error('API call failed: ',errorHM )
+    }
+}
+
+// schedule the api call to run every hour
+cron.schedule('0 * * * *', () => {
+    completeAPICall();
+})
+
+
 // app.use(express.json());
 app.use(express.json({ limit: '10mb'}));
 app.use((requestHM, responseHM, next) =>{
